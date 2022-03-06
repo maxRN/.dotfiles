84512e2eeb679cb65374ec0ab23dab2ee317666c
Max Große <github@maxrn.net>
2022-03-06 12:27:46 +0100

    style(lua): format properly, add tab list keybind and document symbol keybind

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -31,9 +31,10 @@ set nowrap
 " Find files using Telescope command-line sugar.
 nnoremap <C-p> <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>t <cmd>Telescope tele_tabby list<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
 nnoremap <leader>fr <cmd>lua require('telescope.builtin').lsp_references()<cr>
+nnoremap <leader>o <cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>
 nnoremap <leader>hh <cmd>Telescope heading<cr>
 
 " all packer stuff
--- nvim/.config/nvim/lua/telescope_setup.lua
+++ nvim/.config/nvim/lua/telescope_setup.lua
@@ -1,16 +1,21 @@
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('file_browser')
-require('telescope').load_extension("heading")
-require('telescope').setup {
-    pickers = {
-        buffers = {
-            mappings = {
-                i = {
-                    ["<c-d>"] = "delete_buffer",
-                }
-            }
-        }
-    }
-}
-
-
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("file_browser")
+require("telescope").load_extension("heading")
+require("telescope").setup({
+	extensions = {
+		tele_tabby = {
+			use_highlighter = true,
+		},
+	},
+})
+require("telescope").setup({
+	pickers = {
+		buffers = {
+			mappings = {
+				i = {
+					["<c-d>"] = "delete_buffer",
+				},
+			},
+		},
+	},
+})
