9d76e618a6d80ab9c7474ddb2816773375a5e380
Max Große <github@maxrn.net>
2022-04-20 17:39:24 +0200

    feat(luasnip): style + update to use add_snippets

--- nvim/.config/nvim/lua/luasnipConfig.lua
+++ nvim/.config/nvim/lua/luasnipConfig.lua
@@ -1,28 +1,42 @@
-local ls = require "luasnip"
+local ls = require("luasnip")
 local types = require("luasnip.util.types")
 
 require("luasnip.loaders.from_vscode").load()
 
-vim.api.nvim_set_keymap("n", "<leader><leader>s", "<cmd>luafile ~/.config/nvim/lua/luasnipConfig.lua<CR>", { silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader><leader>s",
+	"<cmd>luafile ~/.config/nvim/lua/luasnipConfig.lua<CR>",
+	{ silent = true }
+)
 
-ls.config.set_config {
-    history = true,
+ls.config.set_config({
+	history = true,
 
-    updateevents = "TextChanged, TextChangedI",
+	updateevents = "TextChanged, TextChangedI",
 
-    enable_autosnippets = true,
+	enable_autosnippets = true,
 
-    ext_opts = {
-        [types.choiceNode] = {
-            active = {
-                virt_text = { { "<-", "Error"}},
-            },
-        },
-    },
-}
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "<-", "Error" } },
+			},
+		},
+	},
+})
 
-ls.snippets = {
-    all = {
-        ls.parser.parse_snippet("expand", "--this is what was expanded")
-    }
-}
+local f = ls.function_node
+local s = ls.s
+
+ls.add_snippets(nil, {
+	markdown = {
+		ls.parser.parse_snippet("expand", "# this is markdown expand"),
+		s(
+			"date",
+			f(function()
+				return os.date("%A, %d %B - %Y-%m-%d")
+			end)
+		),
+	},
+})
