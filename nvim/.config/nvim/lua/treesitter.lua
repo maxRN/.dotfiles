0890115bf40e79dcd25ec380465b5adc5138a144
Max Große <github@maxrn.net>
2022-04-27 14:30:13 +0200

    feat: move treesitter config to separate file

---
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -0,0 +1,40 @@
+require("nvim-treesitter.configs").setup({
+	context_commentstring = {
+		enable = false,
+		enable_autocmd = false,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = "all",
+
+	-- Install languages synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	indent = {
+		enable = true,
+	},
+
+	-- List of parsers to ignore installing
+	--  ignore_install = { "javascript" },
+
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
+
+		-- list of language that will be disabled
+		disable = { "c", "rust", "lua" },
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
