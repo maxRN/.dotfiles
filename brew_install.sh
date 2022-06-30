1ccec0b2debfde839347d08cfa975f0d2ad92c98
Max Große <github@maxrn.net>
2022-06-30 11:51:08 +0200

    add brew install script to get up and running

---
+++ brew_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+BREW_PACKAGES="rg bat stow fzf nvim tmux ani-cli btop mpv curl fd ffmpeg neofetch pandoc tree tree-sitter zsh alacritty font-jetbrains-mono iina kdiff3"
+
+brew install ${BREW_PACKAGES}
