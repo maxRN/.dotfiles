891a889253a239814e4f3df60751085a68f01d19
Max Große <max12.gro@gmail.com>
2025-07-01 10:55:12 +0200

    replace markdown rendering plugin

---
+++ home/dotfiles/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        vim.keymap.set("n", "<leader>md", "<Cmd>RenderMarkdown toggle<Cr>")
+    },
+}
