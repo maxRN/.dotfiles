a5006ff4c2f31745a9cafc7cb7fff811615e78fc
Max Große <max12.gro@gmail.com>
2025-08-04 08:15:47 +0200

    better shared config

--- hosts/ohnezahn/default.nix
+++ hosts/ohnezahn/default.nix
@@ -10,47 +10,13 @@ in
   imports = [
     ../../modules/fish-fix.nix
     ./homebrew.nix
+    ../../modules/darwin-settings.nix
   ];
 
   time.timeZone = "Europe/Berlin";
 
   programs = {
     fish.enable = true;
-  };
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system = {
-    primaryUser = "maxrn";
-    stateVersion = 4;
-    defaults = {
-      smb.NetBIOSName = hostname;
-      trackpad.Clicking = true; # enable tap to click
-      dock = {
-        autohide = true;
-        magnification = true;
-        tilesize = 64;
-        show-recents = false;
-        largesize = 80;
-      };
-      NSGlobalDomain = {
-        NSWindowShouldDragOnGesture = true;
-      };
-      # Customize settings that not supported by nix-darwin directly
-      # see the source code of this project to get more undocumented options:
-      #    https://github.com/rgcr/m-cli
-      #
-      # https://daiderd.com/nix-darwin/manual/index.html
-      # All custom entries can be found by running `defaults read` command.
-      # or `defaults read xxx` to read a specific domain.
-      CustomUserPreferences = {
-        "com.apple.desktopservices" = {
-          # Avoid creating .DS_Store files on network or USB volumes
-          DSDontWriteNetworkStores = true;
-          DSDontWriteUSBStores = true;
-        };
-      };
-    };
   };
 
   nixpkgs.hostPlatform = "aarch64-darwin";
@@ -60,7 +26,6 @@ in
     computerName = hostname;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."${username}" = {
     home = "/Users/${username}";
     description = username;
@@ -75,8 +40,5 @@ in
     };
   };
 
-  # Add ability to used TouchID for sudo authentication
-  security.pam.services.sudo_local.touchIdAuth = true;
   environment.shells = [ pkgs.fish ];
-
 }
---
+++ modules/darwin-settings.nix
@@ -0,0 +1,49 @@
+{ config, ... }:
+{
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system = {
+    primaryUser = "maxrn";
+    stateVersion = 4;
+    defaults = {
+      smb.NetBIOSName = config.networking.hostName;
+      trackpad.Clicking = true; # enable tap to click
+      dock = {
+        autohide = true;
+        magnification = true;
+        tilesize = 64;
+        show-recents = false;
+        largesize = 80;
+      };
+      NSGlobalDomain = {
+        # allow pressing Cmd + Control to drag window while clicking anywhere
+        NSWindowShouldDragOnGesture = true;
+        # show hidden files in Finder
+        AppleShowAllFiles = true;
+      };
+      finder = {
+        # delete files in bin after 30 days
+        FXRemoveOldTrashItems = true;
+        # when searching in Finder, use the current folder by default (defaults to entire mac otherwise)
+        FXDefaultSearchScope = "SCcf";
+      };
+      # Customize settings that not supported by nix-darwin directly
+      # see the source code of this project to get more undocumented options:
+      #    https://github.com/rgcr/m-cli
+      #
+      # https://daiderd.com/nix-darwin/manual/index.html
+      # All custom entries can be found by running `defaults read` command.
+      # or `defaults read xxx` to read a specific domain.
+      CustomUserPreferences = {
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+      };
+    };
+  };
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.services.sudo_local.touchIdAuth = true;
+}
