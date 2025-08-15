79572262b30be40f252d743a6855cb751daad890
Max Große <max12.gro@gmail.com>
2025-08-15 13:54:12 +0200

    re-enable treesitter

--- home/dotfiles/nvim/lua/plugins/treesitter.lua
+++ home/dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,21 @@
 vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
 })
+
+local langs = { "go", "kotlin", "rust", "javascript", "typescript" }
+
+require("nvim-treesitter").install(langs)
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = langs,
+    callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
+
+vim.cmd([[:TSUpdate]])
