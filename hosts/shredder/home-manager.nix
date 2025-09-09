89ba629774c2a50467f7c5c784130c2c0e1c9c83
Max Große <max12.gro@gmail.com>
2025-09-09 09:58:39 +0200

    setup for app development

--- hosts/shredder/home-manager.nix
+++ hosts/shredder/home-manager.nix
@@ -9,6 +9,14 @@
     ../../home/darwin.nix
   ];
 
+  home.sessionVariables = {
+    ANDROID_HOME = "$HOME/Library/Android/sdk";
+  };
+
+  home.sessionPath = [
+    "$ANDROID_HOME/platform-tools"
+  ];
+
   home.packages =
     let
       unstable = with pkgs-unstable; [
