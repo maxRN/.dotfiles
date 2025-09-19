21b533eef478296958dc6f115991d06b3565a626
Max Große <max12.gro@gmail.com>
2025-09-19 22:20:43 +0200

    actually enable treesitter highlighting...lol

--- home/dotfiles/nvim/lua/plugins/treesitter.lua
+++ home/dotfiles/nvim/lua/plugins/treesitter.lua
@@ -2,25 +2,37 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
 })
 
-local langs = {
-    "css",
-    "go",
-    "html",
-    "java",
-    "javascript",
-    "kotlin",
-    "nix",
-    "python",
-    "rust",
-    "typescript",
-    "typst",
+local langs_config = {
+    { "css",        "css" },
+    { "go",         "go" },
+    { "html",       "html" },
+    { "java",       "java" },
+    { "javascript", "javascript" },
+    { "kotlin",     "kotlin" },
+    { "nix",        "nix" },
+    { "python",     "python" },
+    { "rust",       "rust" },
+    { "typescript", "typescript" },
+    { "typst",      "typst" },
+    { "tsx",        "typescriptreact" },
+    { "jsx",        "javascriptreact" },
+    { "lua",        "lua" },
 }
+
+local langs = {}
+local patterns = {}
+for _, lang_setup in ipairs(langs_config) do
+    local lang = lang_setup[1]
+    table.insert(langs, lang)
+    local pattern = lang_setup[2]
+    table.insert(patterns, pattern)
+end
 
 require("nvim-treesitter").install(langs)
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = langs,
-    callback = function()
+    pattern = patterns,
+    callback = function(ev)
         -- syntax highlighting, provided by Neovim
         vim.treesitter.start()
         -- folds, provided by Neovim
