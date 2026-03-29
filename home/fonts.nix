e4a6ed663b4cecd30a3b9f587c3b6c8b85c0012e
Max Große <max12.gro@gmail.com>
2026-03-29 11:59:43 +0200

    remove fonts

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
-let
-  berkeley-mono = pkgs.callPackage ../packages/berkeley-mono.nix { };
-in
 {
   home.packages = [
     pkgs.cascadia-code
-    berkeley-mono
   ];
 
   fonts.fontconfig.enable = true;
